a00dc81d5012e48dc9b2b3b31d71cdc167de6f38
hiphil2000 <hiphil2000@naver.com>
2018-05-30 16:33:12 +0900

    서비스 프로그램, 서비스 라이브러리 추가

---
+++ RandomMenuAdvisor_Service/Service.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.ServiceModel;
+using ServiceLibrary;
+
+namespace RandomMenuAdvisor_Service
+{
+    public class Service
+    {
+        private ServiceHost host;
+        private bool isRunning;
+
+        public bool IsRunning { get => isRunning; }
+
+        public Service()
+        {
+            
+        }
+
+        public bool StartService()
+        {
+            host = new ServiceHost(typeof(MenuAdvisorService),
+                                    new Uri("http://192.168.1.69/wcf/example/RandomMenuAdvisor_Service"));
+
+            host.AddServiceEndpoint(
+                typeof(IMenuAdvisor),
+                new BasicHttpBinding(),
+                "");
+
+            
+
+            host.Open();
+            isRunning = true;
+            return true;
+
+        }
+
+        public bool StopService()
+        {
+            host.Close();
+            isRunning = false;
+            return true;
+        }
+    }
+}
---
+++ ServiceLibrary/ServiceLibrary/MenuAdvisorService.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.ServiceModel;
+using System.Data;
+
+namespace ServiceLibrary
+{
+    [ServiceContract(Namespace = "http://192.168.1.69/wcf/MenuAdvisorService")]
+    public interface IMenuAdvisor
+    {
+        [OperationContract]
+        string test();
+
+        [OperationContract]
+        bool GetDatabaseStatus();
+
+        [OperationContract]
+        DataTable GetRandomRequestedData();
+
+        [OperationContract]
+        DataTable GetCategoryData();
+
+        [OperationContract]
+        DataTable GetFoodData();
+
+    }
+
+    public class MenuAdvisorService : IMenuAdvisor
+    {
+        DatabaseService ds;
+        
+        MenuAdvisorService()
+        {
+            ds = new DatabaseService();
+        }
+
+        public DataTable GetCategoryData()
+        {
+            return ds.GetCategoryData();
+        }
+
+        public bool GetDatabaseStatus()
+        {
+            return ds.GetDatabaseStatus();
+        }
+
+        public DataTable GetFoodData()
+        {
+            return ds.GetFoodData();
+        }
+
+        public DataTable GetRandomRequestedData()
+        {
+            return ds.GetRandomRequestedData();
+        }
+
+        public string test()
+        {
+            return "test";
+        }
+    }
+    
+}
