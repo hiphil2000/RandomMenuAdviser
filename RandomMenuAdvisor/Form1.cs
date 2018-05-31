ecdca2288739432dbc413ec5b275820154170d9e
OFFICE-N119 <OFFICE-N119@OFFICE-N126-HP>
2018-05-31 16:28:56 +0900

    소스 수정 및 조건문 작성. DB연동.

--- RandomMenuAdvisor/Form1.cs
+++ RandomMenuAdvisor/Form1.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using RandomMenuAdvisor.ServiceReference1;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -12,47 +13,53 @@ namespace RandomMenuAdvisor
 {
     public partial class RandomMenu : Form
     {
-        string Menu;         // 오늘 저장할 메뉴
-        string Lastmenu;     // 어제 저장한 메뉴
-        string a;
+        string Menu, Lastmenu;         // 오늘 메뉴 저장 변수, 어제 메뉴 저장 변수입니다.
+
+        MenuAdvisorClient client;
+
 
         public RandomMenu()
         {
             InitializeComponent();
-        }
+            client = new MenuAdvisorClient();
 
+        }
 
         /// <summary>
         /// 텍스트 박스에 랜덤으로 음식을 띄우는 부분입니다.
         /// </summary>
         /// <param name="sender"></param>
         /// <param name="e"></param>
-       
+
 
         private void btn_Rec_Click(object sender, EventArgs e)
         {
+            // dgrid_Sta.RowCount = 30; = 그리드뷰 행 수를 30개로 지정
+            //txt_Rec.Text = "밥";
             while (true)
             {
-                Menu = "Select top (1) * from tblname where 통계수치 < 30";
+                // DB select
+                Lastmenu = "Select menu, max(날짜) from tblname group by menu"; // 어제 메뉴를 db에서 불러와서 변수에 넣는 부분입니다.
+                Menu = "Select top (1) * from tblname where 통계수치 < 30";     // 오늘 메뉴를 랜덤으로 db에서 불러와 변수에 넣는 부분입니다.
 
-                //a = "Select a from tblname where menu ="+Menu;
-
-                /*if (Menu == Lastmenu)
-                {
-                    if (a == b)
-                    {
-
-                    }
-                }*/
                 if (Menu != Lastmenu)
                 {
                     txt_Rec.Text = Menu;
+                    // 메뉴 및 시간 서버 전송 (insert, update)
+                    /* 
+                     * string toDayTime = DateTime.Now.ToString("HHmmss");
+                     * int ab = Int32.Parse(toDayTime);
+                     */
                 }
                 else
                 {
-
+                    // 다시 돌아가서 실행
                 }
             }
+        }
+        public void SetDataSource(DataTable dt)
+        {
+            dgrid_Sta.DataSource = dt;
         }
     }
 }
